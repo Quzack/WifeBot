0fc0945c99fb18b0946d9aa828ac9528986e108e
Quzack <85388487+Quzack@users.noreply.github.com>
2021-06-05 14:48:46 +0530

    Update Staff Command.js

--- Wife Bot Commands/Staff Command.js
+++ Wife Bot Commands/Staff Command.js
@@ -6,7 +6,7 @@ module.exports = {
     description: "Displays all online staff",
     execute(message,args,Discord){
         const noStaff = new Discord.MessageEmbed().setColor("##ffffed").addFields({name: "No staff!", value: "There are no online staff available at the moment :("})
-        const Staffrole = "848478990317518858"//staff id
+        const Staffrole = ""//staff id
         var onlineStaff = message.guild.roles.cache.get(Staffrole).members.filter(mem => mem.presence.status === 'online').map(mem => mem.user);
         const StaffList = new Discord.MessageEmbed().setColor("#ffffed").addFields({name: "Online staff", value: "Staff: " + onlineStaff }).setFooter("Pinged by: " + message.author.tag)
         if(!Object.keys(onlineStaff).length > 0) {return message.channel.send(noStaff) }
